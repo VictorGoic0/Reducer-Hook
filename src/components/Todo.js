f42f51e393fc047292d707bf6b6cce1cc163929c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-11 20:26:09 -0400

    Styling and JSX from redux version of a todo project imported (will be refactored).

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { toggleTodo } from "../actions";
+
+class Todo extends Component {
+  toggleTodo = id => {
+    this.props.toggleTodo(id);
+  };
+
+  render() {
+    return (
+      <div
+        onClick={() => this.toggleTodo(this.props.todo.id)}
+        className={this.props.todo.completed ? `cleared` : ``}
+      >
+        <li>{this.props.todo.task}</li>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { toggleTodo }
+)(Todo);
